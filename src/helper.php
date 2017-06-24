bc4f45f008f6a28c5433e9f74e8bb32c682508ed
MohannadNaj <me@mohannad.io>
2017-06-24 08:12:38 +0300

    Code Style Fixes

--- src/helper.php
+++ src/helper.php
@@ -2,11 +2,16 @@
 const ACTIVESTATE_GLOBAL_KEY = '_ACTIVESTATE_GLOBAL_KEY';
 
 if (! function_exists('set_active')) {
+
+
     function set_active(...$params)
     {
         static $loaded = [];
         // default values
-        static $defaults = ['class'=> 'active', 'return' => 'class'];
+        static $defaults = [
+                            'class'  => 'active',
+                            'return' => 'class',
+                           ];
 
         // get the active element
         if (count($params) == 1 && is_string($params[0])) {
@@ -16,28 +21,38 @@ if (! function_exists('set_active')) {
                 return $loaded[$params[0]]['active'];
             }
 
-        // setting the active element, should be one array parameter, e.g: set_active(['tabs'=>'about'])
-        } elseif (count($params) == 1 && is_array($params[0])) {
+            // setting the active element, should be one array parameter, e.g: set_active(['tabs'=>'about'])
+        } else if (count($params) == 1 && is_array($params[0])) {
             return _set_active_add($params, $loaded, $defaults);
-        // if we are checking the active state, e.g: set_active('tabs','about')
-        } elseif (count($params) == 2) {
+            // if we are checking the active state, e.g: set_active('tabs','about')
+        } else if (count($params) == 2) {
             return _set_active_check_state($params, $loaded, $defaults);
         }
-    }
-}
+
+    }//end set_active()
+
+
+}//end if
 
 if (! function_exists('is_active')) {
-    function is_active(... $params)
+
+
+    function is_active(...$params)
     {
         if (count($params) == 1) {
             return set_active(ACTIVESTATE_GLOBAL_KEY, $params[0]);
         } else {
             return set_active($params[0], $params[1]);
         }
-    }
+
+    }//end is_active()
+
+
 }
 
 if (! function_exists('_set_active_check_state')) {
+
+
     function _set_active_check_state(& $params, & $loaded, & $defaults)
     {
         // if we are checking the active state for an index isn't set yet, return false or ''
@@ -50,31 +65,37 @@ if (! function_exists('_set_active_check_state')) {
             // return true if it's return type set to boolean
             if (_set_active_is_boolean($loaded[$params[0]]['return'])) {
                 return true;
-            }
-            // return the class we set before while the return type is class
-            elseif ($loaded[$params[0]]['return'] == 'class') {
+            } //end if
+            else if ($loaded[$params[0]]['return'] == 'class') {
                 return $loaded[$params[0]]['class'];
             }
-        } else { // the first param active element does not match the second param
-            return _set_active_is_boolean($loaded[$params[0]]['return']) ? false: null;
+        } else {
+            // the first param active element does not match the second param
+            return _set_active_is_boolean($loaded[$params[0]]['return']) ? false : null;
         }
-    }
-}
+
+    }//end _set_active_check_state()
+
+
+}//end if
 
 if (! function_exists('_set_active_add')) {
+
+
     function _set_active_add(& $params, & $loaded, & $defaults)
     {
-        $settings = $params[0];
-        $indexKey = null;
+        $settings         = $params[0];
+        $indexKey         = null;
         $preparedSettings = [];
         foreach ($settings as $key => $value) {
             // if the settings isn't the defaults, e.g: set_active(['tabs'=>'about','return'=>'boolean']) or set_active(['tabs'=>'about','class'=>'is-active'])
             if ($key === 'class' || $key === 'return') {
                 $preparedSettings[$key] = $value;
-            } elseif (empty($indexKey)) {
+            } else if (empty($indexKey)) {
                 if (empty($key) && count($settings) <= 3) {
                     $key = ACTIVESTATE_GLOBAL_KEY;
                 }
+
                 $preparedSettings['active'] = $value;
                 $indexKey = $key;
             }
@@ -95,13 +116,24 @@ if (! function_exists('_set_active_add')) {
         }
 
         return null;
-    }
-}
+
+    }//end _set_active_add()
+
+
+}//end if
 
 if (! function_exists('_set_active_is_boolean')) {
+
+
     function _set_active_is_boolean($value)
     {
-        $booleanValues = ['bool','boolean'];
+        $booleanValues = [
+                          'bool',
+                          'boolean',
+                         ];
         return in_array($value, $booleanValues);
-    }
+
+    }//end _set_active_is_boolean()
+
+
 }
--- tests/ActiveStateTest.php
+++ tests/ActiveStateTest.php
@@ -14,24 +14,24 @@ class ActiveStateTest extends PHPUnit_Framework_TestCase
     {
         set_active(['navbar'=>'index']);
 
-        $this->assertEquals(is_active('navbar','index') , "active");
-        $this->assertNull(is_active('navbar','about'));
+        $this->assertEquals(is_active('navbar', 'index'), "active");
+        $this->assertNull(is_active('navbar', 'about'));
     }
 
     public function test_return_custom_css_class()
     {
         set_active(['navbar'=>'index','class'=>'some-active-css-class']);
 
-        $this->assertEquals(is_active('navbar','index'),"some-active-css-class");
-        $this->assertNull(is_active('navbar','about'));
+        $this->assertEquals(is_active('navbar', 'index'), "some-active-css-class");
+        $this->assertNull(is_active('navbar', 'about'));
     }
 
     public function test_return_boolean()
     {
         set_active(['navbar'=>'index','return'=>'boolean']);
 
-        $this->assertTrue(is_active('navbar','index'));
-        $this->assertFalse(is_active('navbar','about'));
+        $this->assertTrue(is_active('navbar', 'index'));
+        $this->assertFalse(is_active('navbar', 'about'));
     }
 
     public function test_global_mode()
@@ -48,14 +48,14 @@ class ActiveStateTest extends PHPUnit_Framework_TestCase
         set_active(['sidebar'=>'dashboard']);
         set_active(['sidebar-submenu'=>'overview','class'=>'submenu-is-active']);
 
-        $this->assertTrue(is_active('navbar','admin'));
-        $this->assertFalse(is_active('navbar','contact'));
+        $this->assertTrue(is_active('navbar', 'admin'));
+        $this->assertFalse(is_active('navbar', 'contact'));
 
-        $this->assertEquals(is_active('sidebar','dashboard'),'active');
-        $this->assertNull(is_active('sidebar','about'));
+        $this->assertEquals(is_active('sidebar', 'dashboard'), 'active');
+        $this->assertNull(is_active('sidebar', 'about'));
 
-        $this->assertEquals(is_active('sidebar-submenu','overview'),'submenu-is-active');
-        $this->assertNull(is_active('sidebar-submenu','charts'));
+        $this->assertEquals(is_active('sidebar-submenu', 'overview'), 'submenu-is-active');
+        $this->assertNull(is_active('sidebar-submenu', 'charts'));
     }
 
     public function test_override_key_and_return_type()
@@ -63,33 +63,32 @@ class ActiveStateTest extends PHPUnit_Framework_TestCase
         set_active(['navbar'=>'index','return'=>'class']);
         set_active(['navbar'=>'about','return'=>'boolean']);
 
-        $this->assertFalse(is_active('navbar','index'));
-        $this->assertTrue(is_active('navbar','about'));
+        $this->assertFalse(is_active('navbar', 'index'));
+        $this->assertTrue(is_active('navbar', 'about'));
     }
 
     public function test_setting_class_while_return_boolean()
     {
         set_active(['navbar'=>'index','return'=>'boolean','class'=>'some-active-css-class']);
 
-        $this->assertTrue(is_active('navbar','index'));
-        $this->assertFalse(is_active('navbar','about'));
+        $this->assertTrue(is_active('navbar', 'index'));
+        $this->assertFalse(is_active('navbar', 'about'));
     }
 
-	public function test_key_not_exist()
+    public function test_key_not_exist()
     {
-  		$this->assertNull(is_active('key_is_not_set','mmm'));
+        $this->assertNull(is_active('key_is_not_set', 'mmm'));
     }
 
     public function test_get_active()
     {
         set_active(['navbar'=>'index']);
 
-        $this->assertEquals(set_active('navbar'),"index");
+        $this->assertEquals(set_active('navbar'), "index");
     }
 
     public function test_get_active_key_not_exist()
     {
         $this->assertNull(is_active('get_active_key_not_exist'));
     }
-
 }
